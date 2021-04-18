0167c0560800e173d65f7d87f39e4024fd9427a9
Soultan Hatsijev <soultan_98@hotmail.com>
2021-04-19 00:31:10 +0200

    4. Layout Changes

--- AppointmentScheduling/AppointmentScheduling/Models/ErrorViewModel.cs
+++ AppointmentScheduling/AppointmentScheduling/Models/ErrorViewModel.cs
@@ -5,7 +5,7 @@ namespace AppointmentScheduling.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
+        // TODO : Ajouter des merdes
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
