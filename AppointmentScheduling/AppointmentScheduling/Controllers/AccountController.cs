f0e37f6286b16987ccbb401a9d58a4a9939ef16f
Soultan Hatsijev <soultan_98@hotmail.com>
2021-04-18 23:14:54 +0200

    2. Add login view model

---
+++ AppointmentScheduling/AppointmentScheduling/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using AppointmentScheduling.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db ??
+                throw new ArgumentNullException(nameof(db));
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
