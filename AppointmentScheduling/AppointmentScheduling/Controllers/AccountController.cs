6fd277c5c006c5dbba96c62f768af9cd1ed5f56e
Soultan Hatsijev <soultan_98@hotmail.com>
2021-04-19 01:21:28 +0200

    13. Registerr post action method

--- AppointmentScheduling/AppointmentScheduling/Controllers/AccountController.cs
+++ AppointmentScheduling/AppointmentScheduling/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
 ﻿using AppointmentScheduling.Data;
+using AppointmentScheduling.Models;
+using AppointmentScheduling.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,11 +13,21 @@ namespace AppointmentScheduling.Controllers
     public class AccountController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(ApplicationDbContext db)
+        public AccountController(ApplicationDbContext db, UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
         {
             _db = db ??
                 throw new ArgumentNullException(nameof(db));
+            _userManager = userManager ??
+                throw new ArgumentNullException(nameof(userManager));
+            _signInManager = signInManager ??
+                throw new ArgumentNullException(nameof(signInManager));
+            _roleManager = roleManager ??
+                throw new ArgumentNullException(nameof(roleManager));
         }
 
         public IActionResult Login()
@@ -24,6 +37,29 @@ namespace AppointmentScheduling.Controllers
 
         public IActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser()
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
             return View();
         }
     }
