3a42e663eecfb6304946236c9013618e291b0639
Soultan Hatsijev <soultan_98@hotmail.com>
2021-04-20 15:37:46 +0200

    8. Modal for add_edit appointment part 1

--- AppointmentScheduling/AppointmentScheduling/Controllers/AppointmentController.cs
+++ AppointmentScheduling/AppointmentScheduling/Controllers/AppointmentController.cs
@@ -20,6 +20,7 @@ namespace AppointmentScheduling.Controllers
         public IActionResult Index()
         {
             ViewBag.DoctorList = _appointmentService.GetDoctorList();
+            ViewBag.PatientList = _appointmentService.GetPatientList();
             return View();
         }
     }
