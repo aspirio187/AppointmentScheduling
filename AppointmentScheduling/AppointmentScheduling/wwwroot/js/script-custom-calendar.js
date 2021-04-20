44908bb207f95ff3917d14db83d8a9bc712da3f5
Soultan Hatsijev <soultan_98@hotmail.com>
2021-04-20 02:28:42 +0200

    6. Show calendar : Now it works with another code

--- AppointmentScheduling/AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -2,20 +2,43 @@
     InitializeCalendar();
 });
 
+/* Code utilisé dans le tutorial partie '6. Show Calendar' cependant, ça ne fonctionne pas, il faut suivre la fonction suivante */
+//function InitializeCalendar() {
+//    try {
+//        $('#calendar').fullCalendar({
+//            timezone: false,
+//            header: {
+//                left: 'prev,next,today',
+//                center: 'title',
+//                right: 'month,agendaWeek,agendaDay'
+//            },
+//            selectable: true,
+//            editable: false
+//        });
+//    }
+//    catch (e) {
+//        alert(e);
+//    }
+//}
+
 function InitializeCalendar() {
     try {
-        $('#calendar').fullCalendar({
-            timezone: false,
-            header: {
-                left: 'prev,next,today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
-            selectable: true,
-            editable: false
-        });
-    }
-    catch (e) {
+        let calendarE1 = document.getElementById('calendar');
+        if (calendarE1 !== null) {
+            let calendar = new FullCalendar.Calendar(calendarE1, {
+                timezone: false,
+                header: {
+                    left: 'prev,next,today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay'
+                },
+                selectable: true,
+                editable: false
+            });
+
+            calendar.render();
+        }
+    } catch (e) {
         alert(e);
     }
 }
