6cc8189746614f5f5b4a8e5f77c4a0ea8bae2f27
Soultan Hatsijev <soultan_98@hotmail.com>
2021-04-24 00:31:26 +0200

    7. Change doctor event

--- AppointmentScheduling/AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -8,25 +8,6 @@ $(document).ready(function () {
     InitializeCalendar();
 });
 
-/* Code utilisé dans le tutorial partie '6. Show Calendar' cependant, ça ne fonctionne pas, il faut suivre la fonction suivante */
-//function InitializeCalendar() {
-//    try {
-//        $('#calendar').fullCalendar({
-//            timezone: false,
-//            header: {
-//                left: 'prev,next,today',
-//                center: 'title',
-//                right: 'month,agendaWeek,agendaDay'
-//            },
-//            selectable: true,
-//            editable: false
-//        });
-//    }
-//    catch (e) {
-//        alert(e);
-//    }
-//}
-
 let calendar;
 
 function InitializeCalendar() {
