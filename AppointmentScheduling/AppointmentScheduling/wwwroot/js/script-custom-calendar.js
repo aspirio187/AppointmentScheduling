4ce372951a4a71e2c61e564c642c2b8dddff555f
Soultan Hatsijev <soultan_98@hotmail.com>
2021-04-24 00:54:19 +0200

    11. Display patient name and status

--- AppointmentScheduling/AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -78,6 +78,13 @@ function onShowModal(obj, isEventDetails) {
         $("#doctorId").val(obj.doctorId);
         $("#patientId").val(obj.patientId);
         $("#id").val(obj.id);
+        $("#lblPatientName").html(obj.patientName);
+        $("#lblDoctorName").html(obj.doctorName);
+        if (obj.isDoctorApproved) {
+            $("#lblStatus").html("Approved");
+        } else {
+            $("#lblStatus").html("Pending")
+        }
     }
     else {
         $("#appointmentDate").val(obj.startStr + " " + new moment().format("hh:mm A"));
