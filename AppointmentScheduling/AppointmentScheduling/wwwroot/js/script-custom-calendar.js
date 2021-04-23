57f1c47b48a08895831b4b606d49e9e74445ea82
Soultan Hatsijev <soultan_98@hotmail.com>
2021-04-24 00:14:50 +0200

    5. Event click in Full Calendar

--- AppointmentScheduling/AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -45,7 +45,7 @@ function InitializeCalendar() {
                 select: function (event) {
                     onShowModal(event, null);
                 },
-                eventDisplay:'block',
+                eventDisplay: 'block',
                 events: function (fetchInfo, successCallback, failureCallback) {
                     $.ajax({
                         url: routeURL + '/api/Appointment/GetCalendarData?doctorId=' + $("#doctorId").val(),
@@ -73,6 +73,9 @@ function InitializeCalendar() {
                             $.notify("Error", "error");
                         }
                     });
+                },
+                eventClick: function (info) {
+                    getEventDetailsByEventId(info.event);
                 }
             });
             calendar.render();
@@ -139,4 +142,21 @@ function checkValidation() {
         $("#appointmentDate").removeClass('error');
     }
     return isValid;
+}
+
+function getEventDetailsByEventId(info) {
+    $.ajax({
+        url: routeURL + '/api/Appointment/GetCalendarDataById/' + info.id,
+        type: 'GET',
+        dataType: 'JSON',
+        success: function (response) {
+            if (response.status === 1 && response.dataEnum != undefined) {
+                onShowModal(response.dataEnum, true);
+            }
+            successCallback(events);
+        },
+        error: function (xhr) {
+            $.notify("Error", "error");
+        }
+    });
 }
